d77de0bcc21491cc3df332d901209f34268ffe3f
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-03-25 20:15:14 +0530

    build | R

--- build.sh
+++ build.sh
@@ -2,9 +2,17 @@
 . build/envsetup.sh
 lunch pixys_lavender-userdebug
 
+# 0 = Vanilla or Gapps
+# 1 = Vanilla and Gapps
+build_gapps=1
+
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+
+exp_gapps() {
+export BUILD_WITH_MINIMAL_GAPPS=true
+}
 
 compile_plox () {
 make pixys -j10
