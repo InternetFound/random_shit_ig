c6f2c595e35484cf51e74fe146bb3543206a2c05
ImSpiDy <SpiDy2713@gmail.com>
2023-04-09 22:01:42 +0000

    derp final | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,9 +2,16 @@
 . build/envsetup.sh
 lunch derp_lavender-userdebug
 
+build_gapps=1
+
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GMS=false
+
+exp_gapps() {
+export WITH_GMS=true
+}
 
 compile_plox () {
 mka derp -j17
