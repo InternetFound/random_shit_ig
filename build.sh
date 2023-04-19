c5b7769e53efa5473d88fb541b1a0da504a333d7
ImPrashantt <prashant33968@gmail.com>
2023-04-19 02:46:26 +0000

    build | V

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 # Normal build steps
 . build/envsetup.sh
-lunch superior_lavender-userdebug
+lunch derp_lavender-userdebug
 
 # 0 = Vanilla or Gapps
 # 1 = Vanilla and Gapps
-build_gapps=1
+#build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-exp_gapps() {
-export BUILD_WITH_GAPPS=true
-}
+#exp_gapps() {
+#export BUILD_WITH_GAPPS=true
+#}
 
 compile_plox () {
-m bacon -j10
+mka derp -j10
 }
