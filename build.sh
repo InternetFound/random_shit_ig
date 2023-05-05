8f91281d0aca1b5ef293acbb2443068f890af155
ImSpiDy <SpiDy2713@gmail.com>
2023-05-05 20:51:30 +0000

    [skip ci]
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,21 +1,19 @@
 # Normal build steps
 . build/envsetup.sh
-lunch bliss_lavender-userdebug
+lunch rr_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 
 build_gapps=1
-export WITH_GMS=false
-export WITH_GAPPS=false
+export GAPPS=""
 
 exp_gapps() {
-export WITH_GMS=true
-export WITH_GAPPS=true
-export BLISS_BUILD_VARIANT=gapps
+export GAPPS=true
+export GAPPS_ARM32=false
 }
 
 compile_plox () {
-make blissify -j17
+mka bacon -j17
 }
